950e148a0cf692062282ebd241023ea9bd97ea70
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2024-09-03 21:01:55 +0300

    Make tasks 1 and 2

---
+++ HW1/Task1/LinkedList.cs
@@ -0,0 +1,80 @@
+﻿using HashNode;
+
+namespace LinkedList
+{
+    public class LinkedList<TKey, TValue>
+    {
+        private HashNode<TKey, TValue> head;
+
+        public void Add(TKey key, TValue value)
+        {
+            HashNode<TKey, TValue> newNode = new HashNode<TKey, TValue>(key, value);
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                HashNode<TKey, TValue> current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newNode;
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            HashNode<TKey, TValue> current = head;
+            HashNode<TKey, TValue> previous = null;
+
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    if (previous == null)
+                    {
+                        head = current.Next;
+                    }
+                    else
+                    {
+                        previous.Next = current.Next;
+                    }
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public TValue Get(TKey key)
+        {
+            HashNode<TKey, TValue> current = head;
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    return current.Value;
+                }
+                current = current.Next;
+            }
+            throw new KeyNotFoundException($"Key '{key}' not found.");
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            HashNode<TKey, TValue> current = head;
+            while (current != null)
+            {
+                if (current.Key.Equals(key))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+    }
+}
---
+++ HW1/Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using HashTable;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HashTable<string, int> hashTable = new HashTable<string, int>(10);
+
+        hashTable.Add("one", 1);
+        hashTable.Add("two", 2);
+        hashTable.Add("three", 3);
+        
+        int valueForTwo = hashTable.Get("two");
+        Debug.Assert(valueForTwo == 2, "Value for 'two' should be 2");
+
+        hashTable.Remove("two");
+
+        bool containsTwo = hashTable.ContainsKey("two");
+        Debug.Assert(containsTwo == false, "HashTable should not contain 'two' after remove");
+        Debug.Assert(hashTable.ContainsKey("one") == true, "HashTable should contain 'one'");
+        Debug.Assert(hashTable.ContainsKey("three") == true, "HashTable should contain 'three'");
+
+        int valueForOne = hashTable.Get("one");
+        Debug.Assert(valueForOne == 1, "Value for 'one' should be 1");
+
+        Console.WriteLine("OK!");
+    }
+}
