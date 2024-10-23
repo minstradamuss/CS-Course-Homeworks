b79cfce3368429c35baaed006c62984fd2951e98
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2024-10-24 00:02:18 +0300

    Fix task5

--- HW8/Task5/Program.cs
+++ HW8/Task5/Program.cs
@@ -3,12 +3,13 @@
     static volatile bool stopFlag = false;
     static object lockObj = new object();
     static double[] partialSums;
-    static int iterationsPerCheck = 1000000; 
+    static int iterationsPerCheck = 1000000;
 
     static void Main(string[] args)
     {
         Console.WriteLine("Введите количество потоков:");
         int threadCount = int.Parse(Console.ReadLine());
+
         partialSums = new double[threadCount];
 
         Thread[] threads = new Thread[threadCount];
@@ -21,6 +22,7 @@
         }
 
         Console.WriteLine("Введите 'stop' для завершения вычислений.");
+
         while (Console.ReadLine() != "stop") { }
 
         stopFlag = true;
@@ -37,28 +39,26 @@
         }
 
         piApproximation *= 4;
-        
+
         Console.WriteLine($"Приближенное значение числа pi: {piApproximation}");
     }
 
     static void CalculatePi(int threadIndex, int threadCount)
     {
         double sum = 0;
-        long start = threadIndex;
-        int sign = (start % 2 == 0) ? 1 : -1;
-        long iterations = 0;
+        long index = threadIndex;
 
         while (!stopFlag)
         {
-            for (long i = start; i < start + iterationsPerCheck * threadCount; i += threadCount)
+            for (long i = index; i < index + iterationsPerCheck * threadCount; i += threadCount)
             {
+                int sign = (i % 2 == 0) ? 1 : -1;
                 sum += sign * (1.0 / (2 * i + 1));
-                sign = -sign;
             }
 
-            start += iterationsPerCheck * threadCount;
+            index += iterationsPerCheck * threadCount;
 
-            if (iterations % iterationsPerCheck == 0 && stopFlag)
+            if (stopFlag)
                 break;
         }
 
