179015e8eb1d328a00398ee5748cc82d172f2cff
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2024-11-11 17:39:15 +0300

    Add task 4

--- HW10/Task4/Program.cs
+++ HW10/Task4/Program.cs
@@ -1,174 +1,105 @@
-﻿using NUnit.Common;
-using NUnit.Framework;
-using NUnitLite;
-using System.Reflection;
+﻿using NUnit.Framework;
+using System.Collections.Concurrent;
+using System.Runtime.ExceptionServices;
 
-namespace MergeSort
+class MergeSorter
 {
-    public class MergeSort
+    private readonly Random rng = new();
+
+    private void SubSort(ref List<int> elementsToSort, int l, int r)
     {
-        private readonly int threads;
-        private readonly int delay;
-
-        public MergeSort(int _threads, int _delay = 500)
-        {
-            threads = _threads;
-            delay = _delay;
-        }
-
-        private void merge<T>(T[] array, int lefBound, int mid, int rigBound) where T : IComparable<T>
-        {
-            T[] sortedCopy = new T[rigBound - lefBound + 1];
-            int lefPtr = lefBound, rigPtr = mid + 1, total = 0;
-
-            while (total < sortedCopy.Length)
-            {
-                if (lefPtr <= mid && (rigPtr > rigBound || array[lefPtr].CompareTo(array[rigPtr]) <= 0))
-                {
-                    sortedCopy[total++] = array[lefPtr++];
-                }
-                else
-                {
-                    sortedCopy[total++] = array[rigPtr++];
-                }
-            }
-
-            Array.Copy(sortedCopy, 0, array, lefBound, sortedCopy.Length);
-        }
-
-        private void mergeSort<T>(T[] array, int lefBound, int rigBound) where T : IComparable<T>
-        {
-            if (lefBound >= rigBound) return;
-
-            int mid = (lefBound + rigBound) / 2;
-            mergeSort(array, lefBound, mid);
-            mergeSort(array, mid + 1, rigBound);
-            merge(array, lefBound, mid, rigBound);
-        }
-
-        public void ParallelSortWithIntermediateMerge<T>(T[] array) where T : IComparable<T>
-        {
-            int len = array.Length;
-            int segmentLen = len / threads;
-            Queue<int> completedSegments = new();
-            object lockObj = new();
-
-            Task[] tasks = new Task[threads];
-            for (int i = 0; i < threads; i++)
-            {
-                int start = i * segmentLen;
-                int end = (i == threads - 1) ? len - 1 : (i + 1) * segmentLen - 1;
-
-                tasks[i] = Task.Run(() =>
-                {
-                    Thread.Sleep(delay);
-                    mergeSort(array, start, end);
-
-                    lock (lockObj)
-                    {
-                        completedSegments.Enqueue(i);
-                        Monitor.Pulse(lockObj);
-                    }
-                });
-            }
-
-            int mergedEnd = -1;
-            T[] sortedArray = new T[len];
-
-            for (int mergedSegments = 0; mergedSegments < threads;)
-            {
-                int segmentToMerge;
-                lock (lockObj)
-                {
-                    while (completedSegments.Count == 0)
-                    {
-                        Monitor.Wait(lockObj);
-                    }
-                    segmentToMerge = completedSegments.Dequeue();
-                }
-
-                int start = segmentToMerge * segmentLen;
-                int end = (segmentToMerge == threads - 1) ? len - 1 : (segmentToMerge + 1) * segmentLen - 1;
-
-                int copyStart = mergedEnd + 1;
-                int copyLength = end - start + 1;
-
-                if (copyStart + copyLength > sortedArray.Length)
-                {
-                    throw new ArgumentException("Недостаточно места в sortedArray для копирования данных.");
-                }
-
-                Array.Copy(array, start, sortedArray, copyStart, copyLength);
-
-                int oldMergedEnd = mergedEnd;
-                mergedEnd += copyLength;
-                merge(sortedArray, 0, oldMergedEnd, mergedEnd);
-
-                mergedSegments++;
-            }
-
-            Array.Copy(sortedArray, array, len);
-        }
-
+        elementsToSort.Sort(l, r - l, Comparer<int>.Default);
+        int delay = rng.Next(500, 1000);
+        Thread.Sleep(delay);
     }
 
-    [TestFixture]
-    public class MergeSortTest
+    private List<int> MergeTask(ref List<int> left, ref List<int> right)
     {
-        [Test]
-        public void Test_SingleThreadedSort()
-        {
-            MergeSort mrg = new(1, 100);
-            int[] array = { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
-            mrg.ParallelSortWithIntermediateMerge(array);
+        var result = new List<int>();
+        int leftIndex = 0, rightIndex = 0;
 
-            int[] expected = { 1, 1, 2, 3, 4, 5, 5, 6, 9 };
-            Assert.That(array, Is.EqualTo(expected));
-        }
+        while (leftIndex < left.Count && rightIndex < right.Count)
+            result.Add(left[leftIndex] < right[rightIndex] ? left[leftIndex++] : right[rightIndex++]);
 
-        [Test]
-        public void Test_MultiThreadedSort()
-        {
-            MergeSort mrg = new(4, 200);
-            int[] array = { 3, 1, 4, 1, 5, 9, 2, 6, 5 };
-            mrg.ParallelSortWithIntermediateMerge(array);
+        while (leftIndex < left.Count) result.Add(left[leftIndex++]);
+        while (rightIndex < right.Count) result.Add(right[rightIndex++]);
 
-            int[] expected = { 1, 1, 2, 3, 4, 5, 5, 6, 9 };
-            Assert.That(array, Is.EqualTo(expected));
-        }
-
-        [Test]
-        public void Test_LargeArray()
-        {
-            MergeSort mrg = new(5, 300);
-            int[] array = new int[1000];
-            Random rand = new Random();
-            for (int i = 0; i < array.Length; i++)
-            {
-                array[i] = rand.Next(1000);
-            }
-
-            int[] expected = (int[])array.Clone();
-            Array.Sort(expected);
-
-            mrg.ParallelSortWithIntermediateMerge(array);
-            Assert.That(array, Is.EqualTo(expected));
-        }
+        return result;
     }
 
-    public class Program
+    public List<int> MergeSort(List<int> elementsToSort, int threads)
     {
-        public static void Main(string[] args)
-        {
-            // НЕ РАБОТАЕТ
-            var writer = new ExtendedTextWrapper(Console.Out);
-            var result = new AutoRun(Assembly.GetExecutingAssembly()).Execute(args, writer, TextReader.Null);
+        int part_array = (elementsToSort.Count + threads - 1) / threads;
+        threads = (elementsToSort.Count + part_array - 1) / part_array;
 
-            Console.WriteLine(writer.ToString());
-            if (result == 0)
-                Console.WriteLine("All tests passed!");
-            else
-                Console.WriteLine("Some tests failed.");
+        var result = new List<int>();
+        var resultLock = new object();
+        var finishedSubtasks = new ConcurrentQueue<int>();
+
+        var subtasks = new List<Task>();
+        for (int i = 0; i < threads; ++i)
+        {
+            int l = i * part_array;
+            int r = Math.Min(elementsToSort.Count, l + part_array);
+            var subTask = Task.Run(() => SubSort(ref elementsToSort, l, r));
+            subTask.ContinueWith(_ =>
+            {
+                lock (resultLock)
+                {
+                    finishedSubtasks.Enqueue(l);
+                    Monitor.Pulse(resultLock);
+                }
+            });
+            subtasks.Add(subTask);
         }
+
+        while (result.Count < elementsToSort.Count)
+        {
+            lock (resultLock)
+            {
+                while (finishedSubtasks.IsEmpty) Monitor.Wait(resultLock);
+
+                if (finishedSubtasks.TryDequeue(out int l))
+                {
+                    int r = Math.Min(l + part_array, elementsToSort.Count);
+                    var sortedBatch = elementsToSort.GetRange(l, r - l);
+                    result = MergeTask(ref result, ref sortedBatch);
+                }
+            }
+        }
+
+        Task.WaitAll(subtasks.ToArray());
+        return result;
+    }
+}
+
+class MergeSortExample
+{
+    public void RunExample(int threads, int arraySize)
+    {
+        var random = new Random();
+        var unsortedArray = Enumerable.Range(0, arraySize).Select(_ => random.Next(0, 100)).ToList();
+
+        Console.WriteLine("Unsorted Array (random numbers): " + string.Join(" ", unsortedArray));
+
+        var sorter = new MergeSorter();
+        var sortedArray = sorter.MergeSort(unsortedArray, threads);
+
+        Console.WriteLine("Sorted Array: " + string.Join(" ", sortedArray));
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of threads (p): ");
+        int threads = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the size of the array (n): ");
+        int arraySize = int.Parse(Console.ReadLine());
+
+        var exampleRunner = new MergeSortExample();
+        exampleRunner.RunExample(threads, arraySize);
     }
 }
